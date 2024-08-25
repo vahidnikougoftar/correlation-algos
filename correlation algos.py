3c4e67c3f418661dd55ee4e20f8a0e4d13d5ec11
Vahid Nikou <Vahid.nikougoftar@gmail.com>
2024-08-25 08:08:03 -0700

    added documentation a bit

--- correlation algos.py
+++ correlation algos.py
@@ -6,9 +6,13 @@ import random
 df = pd.DataFrame(np.random.rand(1000,1),columns=['x'])
 
 def gen_lin(x):
+    '''
+    A function to generate a linearly correlated value based on given feature'''
     return x*0.657 + random.random() * 3
 
 def gen_nonlin(x):
+    '''
+    a function to generate a non-linear relationship'''
     return gen_lin(x)+ 20 * x**2
 
 print('\nLINEAR CORRELATION')
